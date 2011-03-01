49431d21b4e8aff1f8e8e307767b132e956c8090
Marco <marcopede@server12GB.(none)>
2011-03-01 14:41:39 +0100

    added dynamic.h

---
+++ dynamic.h
@@ -0,0 +1,26 @@
+#include <math.h> //for the definition of INFINITY
+
+#ifndef INFINITY
+   #define INFINITY 10000
+#endif
+
+#define maxlab 100
+#define maxnodes 10
+#define ftype float
+
+static ftype scr1[maxnodes*maxlab]; //score for each location
+static ftype sumscr[maxnodes*maxlab]; //sum score for each location
+static int auxpos[maxnodes*maxlab]; //position for each location
+static ftype def[maxnodes*maxlab*maxlab]; //cost of deformation for each location if it is a "good" function distance transform can be used
+//static float maxmarg[maxlab]; //max marginals
+static int numlab;
+static int maxnode;
+
+//void compute(int node,int *pos);
+void initmaxmarginal(int node,int p);//initialize for computing max marginals in node,pos
+ftype maxmarginal(int node,int p,int *pos);//compute the max marginals in node,pos
+ftype map(int *mapos,int node,ftype *maxmarg);//compute map and avrg using max marginals
+void filltables(ftype *pscr,ftype *pdef,int pnodes,int pnumlab);//fill scr,pos,def
+
+
+
